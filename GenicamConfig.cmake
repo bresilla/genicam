8f0203d12e3bb45e7a25a0d24ae3b42b0ad4305c
Trim Bresilla <trim.bresilla@gmail.com>
2025-03-17 21:29:51 +0100

    build: standardize installation paths using CMake variables
    - Add GNUInstallDirs include to use standard installation directory variables
    - Update installation paths to use CMake standard variables instead of custom ones
    - Improve comments and formatting for better readability in CMake config
    - Correct and enhance documentation for Genicam variables in the config file
    - Format changes for consistent list style and structure across different architectures
    - Set the Genicam_INCLUDES variable to the correct include directory
    - Prevent PACKAGE_PREFIX_DIR from being cleared at the end

--- GenicamConfig.cmake
+++ GenicamConfig.cmake
@@ -1,15 +1,20 @@
 # Config file for Genicam
 #
-# It defines the requires genicam libraries as imported targets and defines:
-# - Genicam_TARGETS: list of all imported target with their prefix
-# - Genicam_INCLUDES: list of include directories
-# - Genicam_LIBRARIES: list of libraries
+# This file defines the Genicam imported targets and sets the following cache variables:
+# - Genicam_TARGETS: list of imported targets with namespace.
+# - Genicam_INCLUDES: the include directory for Genicam headers.
+# - Genicam_LIBRARIES: list of Genicam library names.
+# - Genicam_FOUND: flag to indicate Genicam was found.
 
 get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/" ABSOLUTE)
 
 if (UNIX)
-  # try to get architecture from compiler
-  EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} -dumpmachine COMMAND tr -d '\n' OUTPUT_VARIABLE CXX_MACHINE)
+  # Try to get architecture from the compiler
+  EXECUTE_PROCESS(
+    COMMAND ${CMAKE_CXX_COMPILER} -dumpmachine
+    COMMAND tr -d '\n'
+    OUTPUT_VARIABLE CXX_MACHINE
+  )
   string(REGEX REPLACE "([a-zA-Z_0-9]+).*" "\\1" ARCHITECTURE ${CXX_MACHINE})
 elseif (WIN32)
   if ("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
@@ -31,7 +36,8 @@ if ("${ARCHITECTURE}" STREQUAL "arm")
     libLog_gcc494_v3_4
     libMathParser_gcc494_v3_4
     libNodeMapData_gcc494_v3_4
-    libXmlParser_gcc494_v3_4)
+    libXmlParser_gcc494_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/bin/Linux32_ARMhf)
   set(GENICAM_LIB_SUFFIX ".so")
 elseif ("${ARCHITECTURE}" STREQUAL "aarch64")
@@ -42,7 +48,8 @@ elseif ("${ARCHITECTURE}" STREQUAL "aarch64")
     libLog_gcc49_v3_4
     libMathParser_gcc49_v3_4
     libNodeMapData_gcc49_v3_4
-    libXmlParser_gcc49_v3_4)
+    libXmlParser_gcc49_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/bin/Linux64_ARM)
   set(GENICAM_LIB_SUFFIX ".so")
 elseif ("${ARCHITECTURE}" STREQUAL "i686")
@@ -53,7 +60,8 @@ elseif ("${ARCHITECTURE}" STREQUAL "i686")
     libLog_gcc48_v3_4
     libMathParser_gcc48_v3_4
     libNodeMapData_gcc48_v3_4
-    libXmlParser_gcc48_v3_4)
+    libXmlParser_gcc48_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/bin/Linux32_i86)
   set(GENICAM_LIB_SUFFIX ".so")
 elseif ("${ARCHITECTURE}" STREQUAL "x86_64")
@@ -64,19 +72,22 @@ elseif ("${ARCHITECTURE}" STREQUAL "x86_64")
     libLog_gcc48_v3_4
     libMathParser_gcc48_v3_4
     libNodeMapData_gcc48_v3_4
-    libXmlParser_gcc48_v3_4)
+    libXmlParser_gcc48_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/bin/Linux64_x64)
   set(GENICAM_LIB_SUFFIX ".so")
 elseif ("${ARCHITECTURE}" STREQUAL "WIN32_i86")
   set(GENICAM_LIBRARIES
     GCBase_MD_VC141_v3_4
-    GenApi_MD_VC141_v3_4)
+    GenApi_MD_VC141_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/library/CPP/lib/Win32_i86)
   set(GENICAM_LIB_SUFFIX ".lib")
 elseif ("${ARCHITECTURE}" STREQUAL "WIN64_x64")
   set(GENICAM_LIBRARIES
     GCBase_MD_VC141_v3_4
-    GenApi_MD_VC141_v3_4)
+    GenApi_MD_VC141_v3_4
+  )
   set(GENICAM_LIBRARIES_DIR ${PACKAGE_PREFIX_DIR}/library/CPP/lib/Win64_x64)
   set(GENICAM_LIB_SUFFIX ".lib")
 else ()
@@ -94,19 +105,19 @@ foreach(GENICAM_LIB ${GENICAM_LIBRARIES})
 
   set(GENICAM_LIB_FILE "${GENICAM_LIBRARIES_DIR}/${GENICAM_LIB}${GENICAM_LIB_SUFFIX}")
   if (UNIX)
-	  set_target_properties(${GENICAM_NAMESPACE_LIB}
-	      PROPERTIES
-			  INTERFACE_INCLUDE_DIRECTORIES "${PACKAGE_PREFIX_DIR}/library/CPP/include/"
-		      IMPORTED_LOCATION_DEBUG "${GENICAM_LIB_FILE}"
-		      IMPORTED_LOCATION_RELEASE "${GENICAM_LIB_FILE}"
-		      IMPORTED_LOCATION "${GENICAM_LIB_FILE}")
+      set_target_properties(${GENICAM_NAMESPACE_LIB}
+          PROPERTIES
+              INTERFACE_INCLUDE_DIRECTORIES "${PACKAGE_PREFIX_DIR}/library/CPP/include/"
+              IMPORTED_LOCATION_DEBUG "${GENICAM_LIB_FILE}"
+              IMPORTED_LOCATION_RELEASE "${GENICAM_LIB_FILE}"
+              IMPORTED_LOCATION "${GENICAM_LIB_FILE}")
   elseif (WIN32)
-	  set_target_properties(${GENICAM_NAMESPACE_LIB}
-	      PROPERTIES
-			  INTERFACE_INCLUDE_DIRECTORIES "${PACKAGE_PREFIX_DIR}/library/CPP/include/"
-		      IMPORTED_IMPLIB_DEBUG  "${GENICAM_LIB_FILE}"
-		      IMPORTED_IMPLIB_RELEASE "${GENICAM_LIB_FILE}"
-		      IMPORTED_IMPLIB "${GENICAM_LIB_FILE}")
+      set_target_properties(${GENICAM_NAMESPACE_LIB}
+          PROPERTIES
+              INTERFACE_INCLUDE_DIRECTORIES "${PACKAGE_PREFIX_DIR}/library/CPP/include/"
+              IMPORTED_IMPLIB_DEBUG  "${GENICAM_LIB_FILE}"
+              IMPORTED_IMPLIB_RELEASE "${GENICAM_LIB_FILE}"
+              IMPORTED_IMPLIB "${GENICAM_LIB_FILE}")
   endif ()
 endforeach()
 
@@ -114,8 +125,11 @@ add_library(genicam INTERFACE)
 add_library(genicam::genicam ALIAS genicam)
 target_link_libraries(genicam INTERFACE ${GENICAM_NAMESPACED_TARGETS})
 
+# Set cache variables for downstream usage
 set(Genicam_LIBRARIES ${GENICAM_LIBRARIES} CACHE STRING "Genicam libraries")
 set(Genicam_TARGETS ${GENICAM_NAMESPACED_TARGETS} CACHE STRING "Genicam targets with namespace")
-set(Genicam_INCLUDES ${GENICAM_NAMESPACED_TARGETS} CACHE STRING "Genicam targets with namespace")
+# Set Genicam_INCLUDES to the proper include directory
+set(Genicam_INCLUDES "${PACKAGE_PREFIX_DIR}/library/CPP/include" CACHE STRING "Genicam include directory")
 set(Genicam_FOUND ON CACHE BOOL "")
-set(PACKAGE_PREFIX_DIR)
+
+# Do not clear PACKAGE_PREFIX_DIR at the end.
